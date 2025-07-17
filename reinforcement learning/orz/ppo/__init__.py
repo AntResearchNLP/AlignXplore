be3c99943990dd5649c60c9254bac5305657d396
jinalee <91237529+JinaLeejnl@users.noreply.github.com>
2025-07-17 19:48:21 +0800

    update

--- reinforcement learning/orz/ppo/__init__.py
+++ reinforcement learning/orz/ppo/__init__.py
@@ -1,2 +1,3 @@
 from .dataset import PromptDataset
 from .trainer import RayPPOTrainer
+from .trainer_streaming import StreamingRayPPOTrainer
--- reinforcement learning/playground/zero_setting_base.py
+++ reinforcement learning/playground/zero_setting_base.py
@@ -108,8 +108,6 @@ This is the problem:
         return prompt, extra
 
 
-
-
 class GenCustomDataset(PromptDataset):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -139,5 +137,39 @@ This is the problem:
         prompt = prompt_template.render(bos_token=bos_token, prompt=prompt_instruction)
 
         extra = {"answer": dialogue[1]["ground_truth"]["value"], "task": dialogue[0]["task"], "chosen": dialogue[0]["chosen"],"rejected": dialogue[0]["rejected"], "chosen_log": dialogue[0]["chosen_log"], "rejected_log": dialogue[0]["rejected_log"]}
+
+        return prompt, extra
+    
+
+
+class StreamingCustomDataset(PromptDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def process_dialogue(self, dialogue: List):
+        prompt_template_jinja = """\
+{{bos_token}}A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. \
+The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>. User: {{prompt}}
+Assistant: <think>\
+"""
+
+        prompt_instruction_template_jinja = """\
+You must put your answer inside <answer> </answer> tags, i.e., <answer> answer here </answer>.
+This is the problem:
+{{prompt}}
+"""
+
+        assert len(dialogue) == 2, "dialogue must contain 2 items"
+
+        prompt_instruction_template = Template(prompt_instruction_template_jinja)
+        prompt_instruction = prompt_instruction_template.render(prompt=dialogue[0]['history'][0]["value"])
+        prompt_template = Template(prompt_template_jinja)
+        if self.tokenizer.bos_token_id is None:
+            bos_token = ""
+        else:
+            bos_token = self.tokenizer.decode([self.tokenizer.bos_token_id])
+        prompt = prompt_template.render(bos_token=bos_token, prompt=prompt_instruction)
+
+        extra = {"answer": dialogue[0]['history'][0]["answer"], "task": dialogue[0]['history'][0]["task"], "responseA": dialogue[0]['history'][0]["responseA"], "responseB": dialogue[0]['history'][0]["responseB"], "last_pref": "", "history": dialogue[0]['history']}
 
         return prompt, extra
